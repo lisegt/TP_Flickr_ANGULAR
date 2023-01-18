9103dd35b0fa86f64e82fd174e1ced50d4dc294f
Lise Gauthier <lise-gauthier@live.fr>
2023-01-18 11:43:10 +0100

    appel à l'API via http

--- Flickr/src/app/app.component.ts
+++ Flickr/src/app/app.component.ts
@@ -1,18 +1,57 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Flickr';
-  copyright = "@Lise Gauthier";
+  copyright = '@Lise Gauthier';
+  key = 'c79ccfe959473fe0156d3a677f809ed2';
 
   //propriété pour le contenu de l'input
-  tag = ""
+  tag = '';
 
+  //méthode exécutée lors d'un changement d'input
   nouveau_mot_cle(): void {
-    console.log("le nouveau mot est " + this.tag)
+    this.getPhotos().subscribe((resp) => console.log(resp.photos.total));
+  }
+
+  //création de l'objet http
+  constructor(private http: HttpClient) {}
+
+  //gérer les erreurs
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // Erreur client ou réseau.
+      console.error('An error occurred:', error.error);
+    } else {
+      // Le backend a retourné un code d'erreur.
+      // La réponse peut contenir un message d'erreur.
+      console.error(
+        `Erreur du backend avec code ${error.status}, le message est: `,
+        error.error
+      );
+    }
+
+    // Retour d'un observable avec un message d'erreur.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+
+  //Récupérer la liste de photos à partir de l'API
+  private getPhotos(): Observable<any> {
+    let url =
+      'https://api.flickr.com/services/rest?' +
+      'nojsoncallback=1&method=flickr.photos.search&api_key=' +
+      this.key +
+      '&tags=' +
+      this.tag +
+      '&format=json';
+    return this.http.get(url).pipe(catchError(this.handleError));
   }
 }
